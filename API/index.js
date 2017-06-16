4a8eccad21ea29d33791ca3ac08f95647275e1be
pcomo24 <pcomo24@gmail.com>
2017-06-16 09:49:06 -0500

    still fixing routes

--- API/index.js
+++ API/index.js
@@ -5,6 +5,10 @@
 var express = require('express');
 var app =  express();
 var axios = require('axios');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //Handlebars view engine
 app.set('view engine', 'hbs');
@@ -12,15 +16,15 @@ app.use('/axios', express.static('node_modules/axios/dist'));
 
 //backend
 app.get('/', function(req, res) {
-    res.render('home.hbs');
+    res.render('search.hbs');
 });
 app.get('/search.hbs', function(req, res) {
     res.render('search.hbs');
-})
-var api_url;
+});
+var api_url = 'https://api.themoviedb.org/3/search/movie/api_key=7e1972182eb6105c196b67794648a379&';
 app.post('/search_results', function(req, res) {
     .then(function (resultData) {
-        api_url = req.body.search
+        api_url = req.body.search;
         console.log(resultData)
     })
 })
@@ -28,13 +32,11 @@ app.post('/search_results', function(req, res) {
 
 /*var config = {
     params: {
-        url: 'https://api.themoviedb.org/3/search/movie/api_key=7e1972182eb6105c196b67794648a379&' +
         api_key: '7e1972182eb6105c196b67794648a379',
         query: 'fight'
     }
 }*/
 app.get('/api', function(req, res) {
-
     console.log('generating response');
     axios.get(api_url, config)
         .then(function (r) {
@@ -48,6 +50,7 @@ app.get('/api', function(req, res) {
 
 
 //port
-app.listen(8000, function() {
-    console.log('port 8000 live')
+var PORT = process.env.PORT || 8000;
+app.listen(PORT, function() {
+    console.log('port ' + PORT ' is live');
 });
